778249731124bb7c869643b2a2123107ace93cdb
Adam Kosmala <adamkosmala@espeo-kosmala.local>
2019-08-06 09:06:43 +0200

    - making cypress configuration optional - adding package manager options

---
+++ bin/questions.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'includeCypress',
+    message: `Should Cypress configuration be included?`,
+  },
+  {
+    type: 'list',
+    name: 'packageManager',
+    message: 'Which package manager should be used to install dependencies?',
+    choices: ['Yarn', 'NPM'],
+  },
+];
