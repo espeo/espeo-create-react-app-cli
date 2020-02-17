d39eae13454ff6ccd45dd2b6cd289e3cdfae9462
mwojslaw <mwojslaw@hotmail.com>
2020-02-17 10:40:09 +0100

    refactor: change components file ext to tsx (#15)
    Allow other extensions to be handled by generateFile command

--- helpers/generateFile.js
+++ helpers/generateFile.js
@@ -1,51 +1,53 @@
 const fs = require('fs');
+const path = require('path');
 const render = require('consolidate').handlebars.render;
 const mkdirp = require('mkdirp');
 
 const generateFileConfig = {
   testFolderName: 'spec',
-  storeFolderName: 'store'
-}
+  storeFolderName: 'store',
+};
 
-const makeDir = (dir) => {
+const makeDir = dir => {
   if (!fs.existsSync(dir)) {
     mkdirp.sync(dir);
   }
 };
 
-const generateFile = async (params) => {
+const getFileExtension = filePath => {
+  const ext = path.extname(filePath || '').split('.');
+  return ext[ext.length - 1];
+};
+
+const generateFile = async params => {
   const {
     targetName,
     targetPath,
     templateSrc,
     type,
-    shouldMoveToStoreFolder
+    shouldMoveToStoreFolder,
   } = params;
 
-  if (
-    typeof targetName === 'undefined' ||
-    typeof templateSrc === 'undefined'
-  ) return;
+  if (typeof targetName === 'undefined' || typeof templateSrc === 'undefined')
+    return;
 
   if (!fs.existsSync(templateSrc)) {
     return;
   }
 
   const file = fs.readFileSync(templateSrc);
+  const fileExt = getFileExtension(templateSrc);
   let targetDir = `${process.cwd()}${targetPath.replace('.', '')}`;
   const renderFile = () => render(file.toString(), { name: targetName });
 
-  const res = await renderFile()
-    .catch(err => {
-      throw err;
-    });
+  const res = await renderFile().catch(err => {
+    throw err;
+  });
 
   try {
     let fileName = type === 'index' ? type : `${targetName}.${type}`;
     if (fileName.includes('functional') || fileName.includes('class')) {
-      fileName = fileName
-        .replace(/.functional/, '')
-        .replace(/.class/, '');
+      fileName = fileName.replace(/.functional/, '').replace(/.class/, '');
     }
 
     let desiredDir;
@@ -59,9 +61,11 @@ const generateFile = async (params) => {
     }
 
     makeDir(desiredDir);
-    fs.writeFileSync(`${desiredDir}/${fileName}.ts`, res);
+    fs.writeFileSync(`${desiredDir}/${fileName}.${fileExt}`, res);
 
-    console.log(`Successfully generated ${desiredDir}/${fileName}.ts file`);
+    console.log(
+      `Successfully generated ${desiredDir}/${fileName}.${fileExt} file`,
+    );
   } catch (error) {
     console.error(error);
   }
--- templates/component.functional.ts
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-interface Props {}
-
-export const {{capitalize name}} = ({  }: Props) => <div>{{capitalize name}} component works!</div>;
