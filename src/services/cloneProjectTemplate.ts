85990acd8b4b5bdc7af4e258ae782574dff88787
Maciej Wojsław <maciej.wojslaw@espeo.eu>
2020-02-20 20:14:53 +0100

    feat: compare local and remote packages versions

--- src/services/cloneProjectTemplate.ts
+++ src/services/cloneProjectTemplate.ts
@@ -3,14 +3,47 @@ import { projectTemplateRepositoryUrl } from 'config';
 import path from 'path';
 import fs from 'fs';
 import { Spinner } from 'cli-spinner';
+import fetch from 'node-fetch';
+
+const getRawFileUrl = (repositoryUrl: string, fileName: string): string =>
+  repositoryUrl
+    .replace('github.com', 'raw.githubusercontent.com')
+    .replace(/.git$/, `/master/${fileName}`);
+
+const getRemotePackageVersion = async (): Promise<string> => {
+  const response = await fetch(
+    getRawFileUrl(projectTemplateRepositoryUrl, 'package.json'),
+  );
+
+  const { version } = await response.json();
+
+  return version;
+};
+
+const getLocalPackageVersion = (projectTemplateSrc: string): string => {
+  const { version: currentVersion } = JSON.parse(
+    fs.readFileSync(path.join(projectTemplateSrc, 'package.json')).toString(),
+  );
+
+  return currentVersion;
+};
 
 export const cloneProjectTemplate = async (): Promise<void> => {
   const projectTemplateSrc = path.join(
     getTemplatesDirectory(),
-    'projectTemplate',
+    'packageTemplate',
   );
 
-  if (fs.existsSync(projectTemplateSrc)) return;
+  const directoryExists = fs.existsSync(projectTemplateSrc);
+
+  if (directoryExists) {
+    const localVersion = getLocalPackageVersion(projectTemplateSrc);
+    const remoteVersion = await getRemotePackageVersion();
+
+    if (localVersion === remoteVersion) return;
+
+    await exec(`rm -rf ${projectTemplateSrc}`);
+  }
 
   const spinnerInstance = new Spinner('Cloning template repository.... %s');
   spinnerInstance.setSpinnerString('|/-\\');
