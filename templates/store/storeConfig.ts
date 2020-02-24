80820a2370da73db0ac86e450d5ece0b994fda80
Maciej Wojsław <maciej.wojslaw@espeo.eu>
2020-02-24 11:26:37 +0100

    fix: changes to store template

--- templates/store/storeConfig.ts
+++ templates/store/storeConfig.ts
@@ -3,15 +3,16 @@ import logger from 'redux-logger';
 {{#ifEquals middleware supportedReduxMiddlewares.reduxObservable}}
 import { createEpicMiddleware } from 'redux-observable';
 {{/ifEquals}}
+{{#ifEquals middleware supportedReduxMiddlewares.reduxSaga}}
 import createSagaMiddleware from 'redux-saga';
-​
+​{{/ifEquals}}
 import { getArticlesService } from '@core/services';
 import { MainArticlesState } from '@pages/MainArticles/namespace';
 import { MainActions } from '@pages/MainArticles/store/actions';
-​
 import rootReducer from './rootReducer';
+{{#ifEquals middleware supportedReduxMiddlewares.reduxSaga}}
 import rootSaga from './rootSaga';
-​
+​{{/ifEquals}}
 {{#ifEquals middleware supportedReduxMiddlewares.reduxObservable}}
 import { rootEpic } from './rootEpic';
 {{/ifEquals}}
@@ -24,6 +25,7 @@ export interface RootStore {
 const dependencies = {
   getArticlesService,
 };
+
 export type Dependencies = typeof dependencies;
 ​
 const epicMiddleware = createEpicMiddleware<
@@ -34,30 +36,30 @@ const epicMiddleware = createEpicMiddleware<
 >({
   dependencies,
 });
+
 {{/ifEquals}}
-​
+{{#ifEquals middleware supportedReduxMiddlewares.reduxSaga}}
 const sagaMiddleware = createSagaMiddleware();
-​
+
+{{/ifEquals}}
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-​
+
 {{#ifEquals middleware supportedReduxMiddlewares.reduxObservable}}
-  const middleware = [epicMiddleware, logger];
+const middleware = [epicMiddleware, logger];
 {{/ifEquals}}
-​
 {{#ifEquals middleware supportedReduxMiddlewares.reduxSaga}}
-  const middleware = [sagaMiddleware, logger];
+const middleware = [sagaMiddleware, logger];
 {{/ifEquals}}
-​
+
 export const rootStore = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(...middleware)),
 );
-​
+
 {{#ifEquals middleware supportedReduxMiddlewares.reduxObservable}}
-  epicMiddleware.run(rootEpic);
+epicMiddleware.run(rootEpic);
 {{/ifEquals}}
-​
 {{#ifEquals middleware supportedReduxMiddlewares.reduxSaga}}
- sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(rootSaga);
 {{/ifEquals}}
